dcdfcfc568b7e2bf335d1745e2cd05625fa8a566
joein <panchuk.george@outlook.com>
2021-08-04 16:11:21 +0300

    fix: make build_and_test executable

--- travis/build_and_test.sh
+++ travis/build_and_test.sh
@@ -3,7 +3,7 @@ set -e
 
 docker build -t justjoinme/cian-real-estate-market-analysis-test:latest -f DockerfileTest .
 docker run -it justjoinme/cian-real-estate-market-analysis-test:latest
-docker build justjoinme/cian-real-estate-market-analysis:latest -f Dockerfile .
+docker build -t justjoinme/cian-real-estate-market-analysis:latest -f Dockerfile .
 
 
 
