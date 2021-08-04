d3235c95344c737fb55a97ccd68e6468499c8117
joein <panchuk.george@outlook.com>
2021-08-04 17:29:30 +0300

    chore: join build and push image

--- travis/build_and_test.sh
+++ travis/build_and_test.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e
 
-docker build -t justjoinme/cian-real-estate-market-analysis-test:latest -f DockerfileTest .
-docker run -it justjoinme/cian-real-estate-market-analysis-test:latest
-docker build -t justjoinme/cian-real-estate-market-analysis:latest -f Dockerfile .
+docker build -t $DOCKER_USERNAME/cian-real-estate-market-analysis-test:latest -f DockerfileTest .
+docker run -it $DOCKER_USERNAME/cian-real-estate-market-analysis-test:latest
 
+docker build -t $DOCKER_USERNAME/cian-real-estate-market-analysis:latest -f Dockerfile .
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker push $DOCKER_USERNAME/cian-real-estate-market-analysis:latest
 
 
 
